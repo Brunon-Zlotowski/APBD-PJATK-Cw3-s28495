b945c82091cc93c2c1c81ad816a4ce54efa0de67
Brunon-Zlotowski <116455129+Brunon-Zlotowski@users.noreply.github.com>
2026-05-08 18:01:10 +0200

    Pokoje i Rezerwacje

---
+++ APBD-PJATK-Cw3-s28495/Models/Reservation.cs
@@ -0,0 +1,32 @@
+﻿namespace APBD_PJATK_Cw3_s28495.Models;
+using System.ComponentModel.DataAnnotations;
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; } = "planned";
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime musi być późniejsze niż StartTime",
+                new[] { nameof(EndTime) });
+        }
+    }
+}
